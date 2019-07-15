a9537fa160f494d1740c3c3a9fda85295edeae0a
yehia hacker <42983220+yehia-hacker@users.noreply.github.com>
2019-07-15 10:36:09 -0400

    Add files via upload

---
+++ update.rb
@@ -0,0 +1,20 @@
+
+puts '-------------------'.green.bold
+puts ''' 
+
+         '''
+puts '''
+1 = Update
+'''.cyan.bold
+puts '''
+2- No update'''.red.bold
+puts '''--------------------'''.yellow.bold
+up = gets.chomp.to_i
+if up == 1
+system('rm -rf /data/data/com.termux/files/home/GSM-CALL')
+system("git clone https://github.com/yehia-hacker/GSM-CALL")
+system('cd /data/data/com.termux/files/home/GSM-CALL;chmod +x *;Ruby GSM-CALL.rb')
+end
+if up == 2
+puts'GOOD Wait'
+end
